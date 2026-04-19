be64878974b43d589d248ee8f490acdd9c4c62ed
mac msm691 <marley.sedlak-martin@epitech.eu>
2026-04-19 20:12:49 +0200

    feat(server): update header with POSIX init prototypes

--- include/server.h
+++ include/server.h
@@ -8,8 +8,11 @@
 #ifndef SERVER_H_
     #define SERVER_H_
 
+    #include <stdint.h>
     #include "protocol.h"
 
-int server_main(int argc, char **argv);
+    int server_main(int argc, char **argv);
+    int init_server(uint16_t port, int *server_fd);
+    int set_non_blocking(int fd);
 
 #endif /* !SERVER_H_ */
